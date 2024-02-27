33f431fc393eb44b376dbe57848fa63cfc544e58
Ian Giestas Pauli <iangiestaspauli@gmail.com>
2024-02-27 15:54:51 -0300

    Update compila.sh

--- compila.sh
+++ compila.sh
@@ -1,2 +1,2 @@
+# isso gera o modulo SO "integra_python"
 gfortran -o integra_python.so integra_python.f95 integra_python.c -I/usr/include/python3.6 -fPIC -shared
-#gfortran -o integra_python.so integra_python.o -I/usr/include/python3.6
